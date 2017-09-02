c9fe75126c74d671a5c0d73ebd2c0b09603a4599
fenrir <fenrir.naru@gmail.com>
2017-09-02 21:35:50 +0900

    Add System_3D class to gather common scheme for store and access.

--- MOMO1/ins_gps/coordinate.rb
+++ MOMO1/ins_gps/coordinate.rb
@@ -6,80 +6,91 @@ coordinate.hのruby版
 $: << File::dirname(__FILE__) unless $:.include?(File::dirname(__FILE__))
 require 'WGS84'
 
-class System_LLH
-  attr_accessor :latitude, :longitude, :height
+class System_3D
+  def initialize(*v3)
+    @buf = [0, 1, 2].collect{|i|
+      v3[i] || 0.0
+    }
+  end
+  def to_a
+    @buf
+  end
+  
+  def abs2
+    return @buf.inject(0.0){|sum, v| sum + (v ** 2)}
+  end
+  def abs
+    return Math::sqrt(abs2())
+  end
+  def [](i)
+    @buf[i]
+  end
+end
+
+class System_LLH < System_3D
+  [:latitude, :longitude, :height].each_with_index{|k, i|
+    class_eval(<<-__TEXT__)
+      def #{k}; @buf[#{i}]; end
+      def #{k}=(v); @buf[#{i}] = v; end
+    __TEXT__
+  }
   alias :lat :latitude
   alias :long :longitude
   alias :lng :longitude
   alias :h :height
   
-  def initialize(*lat_lng_h)
-    lat_lng_h.flatten!(1)
-    @latitude = lat_lng_h.shift || 0.0
-    @longitude = lat_lng_h.shift || 0.0
-    @height = lat_lng_h.shift || 0.0
-  end
+  undef_method :abs2, :abs
   
-  def to_ary
-    return [@latitude, @longitude, @height]
+  def initialize(*lat_lng_h)
+    super(*lat_lng_h)
   end
 end
 
-class System_XYZ
+class System_XYZ < System_3D
   EARTH = WGS84
   F = EARTH::F_E
   A = EARTH::R_E
   B = A * (1.0 - F)
   E = Math::sqrt(F * (2.0 - F))
   
-  attr_accessor :x, :y, :z
+  [:x, :y, :z].each_with_index{|k, i|
+    class_eval(<<-__TEXT__)
+      def #{k}; @buf[#{i}]; end
+      def #{k}=(v); @buf[#{i}] = v; end
+    __TEXT__
+  }
   
   def initialize(*x_y_z)
-    x_y_z.flatten!(1)
-    @x = x_y_z.shift || 0.0
-    @y = x_y_z.shift || 0.0
-    @z = x_y_z.shift || 0.0
-  end
-  
-  def abs2
-    return @x ** 2 + @y ** 2 + @z ** 2
-  end
-  
-  def abs
-    return Math::sqrt(abs2())
+    super(*x_y_z)
   end
   
   def dist(xyz)
-    return Math::sqrt((@x - xyz.x) ** 2 + (@y - xyz.y) ** 2 + (@z - xyz.z) ** 2)
+    return Math::sqrt((x() - xyz.x()) ** 2 + (y() - xyz.y()) ** 2 + (z() - xyz.z()) ** 2)
   end
   
   #
   # 緯度、経度、高度に変換
   #
   def llh
-    if @x == 0 and @y == 0 and @z == 0 then
+    if x() == 0 and y() == 0 and z() == 0 then
       return System_LLH::new(0, 0, -A)
     end
     
     h = A ** 2 - B ** 2;
-    p = Math::sqrt(@x ** 2 + @y ** 2)
-    t = Math::atan2(@z * A, p * B)
+    p = Math::sqrt(x() ** 2 + y() ** 2)
+    t = Math::atan2(z() * A, p * B)
     sint = Math::sin(t)
     cost = Math::cos(t)
-    lat = Math::atan2(@z + (h / B * (sint ** 3)), p - (h / A * (cost ** 3))) 
+    lat = Math::atan2(z() + (h / B * (sint ** 3)), p - (h / A * (cost ** 3))) 
     n = A / Math::sqrt(1.0 - (E ** 2) * (Math::sin(lat) ** 2))
     return System_LLH::new( \
         lat, \
-        Math::atan2(@y, @x), \
+        Math::atan2(y(), x()), \
         p / Math::cos(lat) - n)
   end
   
   def xyz
     return self
-  end
-  
-  def to_ary
-    return [@x, @y, @z]
   end
 end
 
@@ -95,46 +106,44 @@ class System_LLH
   end
   
   def xyz
-    n = A / Math::sqrt(1.0 - (E ** 2) * (Math::sin(@latitude) ** 2))
-    latc, lats = [:cos, :sin].collect{|f| Math.send(f, @latitude)}
-    lngc, lngs = [:cos, :sin].collect{|f| Math.send(f, @longitude)}
+    n = A / Math::sqrt(1.0 - (E ** 2) * (Math::sin(lat()) ** 2))
+    latc, lats = [:cos, :sin].collect{|f| Math.send(f, lat())}
+    lngc, lngs = [:cos, :sin].collect{|f| Math.send(f, lng())}
     return System_XYZ::new( \
-        (n + @height) * latc * lngc, \
-        (n + @height) * latc * lngs, \
-        (n * (1.0 - (E ** 2)) + @height) * lats)
+        (n + h()) * latc * lngc, \
+        (n + h()) * latc * lngs, \
+        (n * (1.0 - (E ** 2)) + h()) * lats)
   end
 end
 
-class System_ENU
-  attr_accessor :east, :north, :up
+class System_ENU < System_3D
+  [:east, :north, :up].each_with_index{|k, i|
+    class_eval(<<-__TEXT__)
+      def #{k}; @buf[#{i}]; end
+      def #{k}=(v); @buf[#{i}] = v; end
+    __TEXT__
+  }
   alias :e :east
   alias :n :north
   alias :u :up
   
   def initialize(*east_north_up)
-    east_north_up.flatten!(1)
-    @east = east_north_up.shift || 0.0
-    @north = east_north_up.shift || 0.0
-    @up = east_north_up.shift || 0.0
-  end
-  
-  def to_ary
-    return [@east, @north, @up]
+    super(*east_north_up)
   end
   
   def System_ENU.relative(pos, base, options = {})
     base_llh = base.llh()
-    rel_x, rel_y, rel_z = pos.xyz().to_ary()
+    rel_x, rel_y, rel_z = pos.xyz().to_a()
     
     unless options[:pos_is_xyz] then
       base_xyz = base.xyz()
-      rel_x -= base_xyz.x
-      rel_y -= base_xyz.y
-      rel_z -= base_xyz.z
+      rel_x -= base_xyz.x()
+      rel_y -= base_xyz.y()
+      rel_z -= base_xyz.z()
     end
     
-    s1, c1 = [:sin, :cos].collect{|f| Math.send(f, base_llh.longitude)}
-    s2, c2 = [:sin, :cos].collect{|f| Math.send(f, base_llh.latitude)}
+    s1, c1 = [:sin, :cos].collect{|f| Math.send(f, base_llh.lng())}
+    s2, c2 = [:sin, :cos].collect{|f| Math.send(f, base_llh.lat())}
     
     return System_ENU::new( \
         -rel_x * s1 + rel_y * c1, \
@@ -150,27 +159,19 @@ class System_ENU
     base_llh = base.llh()
     base_xyz = base.xyz()
     
-    s1, c1 = [:sin, :cos].collect{|f| Math.send(f, base_llh.longitude)}
-    s2, c2 = [:sin, :cos].collect{|f| Math.send(f, base_llh.latitude)}
+    s1, c1 = [:sin, :cos].collect{|f| Math.send(f, base_llh.lng())}
+    s2, c2 = [:sin, :cos].collect{|f| Math.send(f, base_llh.lat())}
     
     return System_XYZ::new( \
-        base_xyz.x - @east * s1 - @north * c1 * s2 + @up * c1 * c2, \
-        base_xyz.y + @east * c1 - @north * s1 * s2 + @up * s1 * c2, \
-        base_xyz.z + @north * c2 + @up * s2)
-  end
-  
-  def abs2
-    return (@east ** 2 + @north ** 2 + @up ** 2)
-  end
-  
-  def abs
-    return Math::sqrt(abs2())
+        base_xyz.x() - east() * s1 - north() * c1 * s2 + up() * c1 * c2, \
+        base_xyz.y() + east() * c1 - north() * s1 * s2 + up() * s1 * c2, \
+        base_xyz.z() + north() * c2 + up() * s2)
   end
   
   def elevation
-    return Math::atan2(@up, Math::sqrt((@east ** 2) + (@north ** 2)))
+    return Math::atan2(up(), Math::sqrt((east() ** 2) + (north() ** 2)))
   end
   def azimuth
-    return Math::atan2(@east, @north)
+    return Math::atan2(east(), north())
   end
 end
